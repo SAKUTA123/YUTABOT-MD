d73c3e7d15e056741303dc66c1462e2829f5e791
Profile <91337596+SAKUTA123@users.noreply.github.com>
2022-07-12 12:13:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let fs = require('fs')
 
-global.kontak = ['17608914335'] //Jangan Sembarang Ganti 
-global.owner = ['17608914335','62895612153565'] // Put your number here
-global.linkGC = ['https://chat.whatsapp.com/EZT51mPq69162pfM3afL1h'] // ganti jadi group lu
+global.kontak = ['6287752078797'] //Jangan Sembarang Ganti 
+global.owner = ['6287752078797','6287760550924'] // Put your number here
+global.linkGC = ['belum ada'] // ganti jadi group lu
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
